afbddb264eb74bedf25b7fa0f6c7ce847949fea4
Breno Leite <brenolleite@gmail.com>
2017-08-08 00:16:55 -0300

    Adding question 3

--- p0-XX-YY/src/main.py
+++ p0-XX-YY/src/main.py
@@ -14,13 +14,32 @@ def swapRedBlue(img):
     return img
 
 def monochromeGreen(img):
-    img[:,:,0] = img[:,:,2] = 0
+    img = img[:,:,2]
     cv2.imwrite('../output/p0-2-b-0.jpg', img)
     return img
 
 def monochromeRed(img):
-    img[:,:,1] = img[:,:,2] = 0
+    img = img[:,:,2]
     cv2.imwrite('../output/p0-2-c-0.jpg', img)
+
+def insertImage(imgA, imgB):
+    heightA, widthA = imgA.shape
+    yiA = int(heightA/2) - 50
+    yfA = int(heightA/2) + 50
+    xiA = int(widthA/2) - 50
+    xfA = int(widthA/2) + 50
+
+    heightB, widthB, depth = imgB.shape
+    yiB = int(heightB/2) - 50
+    yfB = int(heightB/2) + 50
+    xiB = int(widthB/2) - 50
+    xfB = int(widthB/2) + 50
+
+    imgB[yiB:yfB, xiB:xfB, 0] = imgA[yiA:yfA, xiA:xfA]
+    imgB[yiB:yfB, xiB:xfB, 1] = imgA[yiA:yfA, xiA:xfA]
+    imgB[yiB:yfB, xiB:xfB, 2] = imgA[yiA:yfA, xiA:xfA]
+
+    cv2.imwrite('../output/p0-3-0.jpg', imgB)
 
 imgA = swapRedBlue(input)
 
@@ -28,4 +47,4 @@ imgB = monochromeGreen(input)
 
 monochromeRed(input)
 
-
+insertImage(imgB, input)
