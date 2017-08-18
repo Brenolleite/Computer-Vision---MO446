f38515df40d45596e7678d9bccc063a96a440454
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2017-08-18 15:22:19 -0300

    Antes de trocar RAM

--- p1-XX-YY/src/main.py
+++ p1-XX-YY/src/main.py
@@ -1,16 +1,33 @@
 import cv2
+import math
 import copy as cp
 import numpy as np
 
-input = cv2.imread('./input/input.jpg')
+# Python uses the BGR color scheme
+
+input = cv2.imread('../input/input.jpg')
 
 # Gaussian pyramid
 
-def pyrUp(img):
-  testImg = cv2.pyrUp(img)
+def pyrDown(img, lvls):
+    pyramid = np.array
 
-cv2.imwrite('./output/CV2_p1-2-2-0.jpg', img)
+    for i in range(0, lvls):
+        i_height, i_width, i_channels = img.shape
+        height = math.floor(i_height / 2)
+        width = math.floor(i_width / 2)
+        newImg = np.zeros((height, width, 3), np.uint8)
 
-#def pyrDown(img):
+        for j in range(0, height):
+            for l in range(0, width):
+                newImg.itemset((j, l, 0), img.item(j * 2, l * 2, 0))
+                newImg.itemset((j, l, 1), img.item(j * 2, l * 2, 1))
+                newImg.itemset((j, l, 2), img.item(j * 2, l * 2, 2))
 
-pyrUP(cp.copy(input))
+        cv2.imwrite('../output/p1-2-2-{}.jpg'.format(i), newImg)
+        img = newImg
+
+# def Access method to obtain a given level
+# def Pyramid construction needs the image source and the number of levels, one to go UP and one to go DOWN, maybe I should insert this information in the pyrX methods
+
+pyrDown(cp.copy(input), 3)
