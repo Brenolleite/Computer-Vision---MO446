a3922c427b9e525ea0c2bc4c51a3953f9ad4eaa7
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2017-08-29 21:28:29 -0300

    Commented the code

--- p1-XX-YY/src/bi_interpolation.py
+++ p1-XX-YY/src/bi_interpolation.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import copy as cp
 
+# Caller function to the actual interpolation
 def interpolate(input):
     output = cp.copy(input)
 
@@ -14,12 +15,15 @@ def interpolate(input):
 
     return output
 
+# Performs interpolation on a single image channel
 def interpolation(input, output, channel):
     i_height, i_width = input.shape
 
+    # Goes over the entire image
     for i in range(1, i_height, 2):
         for j in range(1, i_width, 2):
 
+            # Calculus for the three pixels that will be changed
             # Up
             if (j + 1 < i_width):
                 mean = math.floor((input.item(i - 1, j - 1) + input.item(i - 1, j + 1)) / 2)
--- p1-XX-YY/src/place_pyramid.py
+++ p1-XX-YY/src/place_pyramid.py
@@ -3,6 +3,7 @@ import numpy as np
 import gaussian_pyramid as gPyr
 import math
 
+# Caller to generate the laplacian pyramid, the last level is added in here
 def placePyramid(img, lvl):
     lPyramid = []
     gPyramid = gPyr.gaussianPyramid(img, lvl)
@@ -20,6 +21,8 @@ def placePyramid(img, lvl):
 
     return lPyramid
 
+# Function that generates a half sized image of the input, result is the sharp
+# details of input
 def pyrContract(current_img, low_pass):
     while low_pass.shape[0] < current_img.shape[0]:
         current_img = np.delete(current_img,(-1),axis=0)
@@ -32,12 +35,17 @@ def pyrContract(current_img, low_pass):
     for i in range(aux.shape[0]):
         for j in range(aux.shape[1]):
             for k in range(aux.shape[2]):
+                # Subtract every pixel from a blured version of it, result are
+                # the sharps details
                 pixel = aux.item(i, j, k) - low_pass.item(i, j, k)
                 aux.itemset((i, j, k), pixel)
 
     return aux
 
+# Function that generates a double sized image of the input, the result is the
+# way towards the original image
 def pyrExpand(current_img, high_pass):
+    # Uses the gaussian expansion
     low_pass = gPyr.pyrExpand(current_img)
 
     while low_pass.shape[0] < high_pass.shape[0]:
@@ -45,6 +53,7 @@ def pyrExpand(current_img, high_pass):
     while low_pass.shape[1] < high_pass.shape[1]:
         high_pass = np.delete(high_pass, (-1), axis = 1)
 
+    # Sums the expanded image with the sharp details saved previously
     aux = low_pass + high_pass
 
     return aux
