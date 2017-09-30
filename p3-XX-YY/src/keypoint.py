2a9b15ec9e025f1ec278344a23bcf6c4e861e229
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2017-09-30 12:01:31 -0300

    Commited from the wrong folder

--- p3-XX-YY/src/keypoint.py
+++ p3-XX-YY/src/keypoint.py
@@ -1,13 +1,23 @@
+import numpy as np
 import cv2
 
-def sift(img):
-
-    # Convert image to black and white
+# Return a list of tuples [(x, y)] of each keypoint
+def kp(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 
-    # Get the keypoints and descriptors
-    sift = cv2.xfeatures2d.SIFT_create()
-    keyPoints, descriptors = sift.detectAndCompute(gray, None)
+    kp = []
+    threshold = 0.01 * dst.max()
+    for i in range(dst.shape[0]):
+        for j in range(dst.shape[1]):
+            if dst[i][j] > threshold:
+                kp.append((i, j))
 
-    # Return the descriptors
-    return (keyPoints, descriptors)
+    return kp
+
+# DEBUG
+video = cv2.VideoCapture('../input/input.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+ret, frame = video.read()
+kp(frame)
