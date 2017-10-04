1da6aa81447a4a0b441b5d77f6d50e5cf0cea2b2
Breno Leite <brenolleite@gmail.com>
2017-10-04 15:43:46 -0300

    Commiting utils and opencv flows

---
+++ p3-XX-YY/src/opencv.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+
+import keypoint
+import utils
+
+def KLT(video_path):
+    # Get video length
+    video = cv2.VideoCapture(video_path)
+    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 100,
+                        qualityLevel = 0.3,
+                        minDistance = 7,
+                        blockSize = 7 )
+
+    # Parameters for lucas kanade optical flow
+    lk_params = dict(winSize  = (15,15),
+                     maxLevel = 2,
+                     criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+    # Get frames
+    ret, frame1 = video.read()
+
+    # Transform frame to grayscale
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+
+    # Get keypoints
+    kps = []
+    kp = cv2.goodFeaturesToTrack(frame1, mask = None, **feature_params)
+
+    kps.append(kp.squeeze())
+
+    for i in range(0, length - 1):
+        # Get frames
+        ret, frame2 = video.read()
+
+        # Transform frame to grayscale
+        frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+        # Calculate optical flow
+        kp, st, err = cv2.calcOpticalFlowPyrLK(frame1, frame2, kps[i-1], None, **lk_params)
+
+        # Add new points to matrix of frames
+        kps.append(kp.squeeze())
+
+        # Search index of bad kp
+        st = np.array(st)
+        index = np.where(st==0)
+
+        # Delete bad keypoints
+        np.delete(kps, index, axis=1)
+
+        # update frame
+        frame1 = frame2
+
+        i += 1
+
+    video.release()
+
+    return kps
+
+
+
+kps = KLT('../input/input2.mp4')
+utils.videoFlow(kps, '../input/input2.mp4', '../output/opencv_flow.avi', (13, 94, 1))
+
--- p3-XX-YY/src/utils.py
+++ p3-XX-YY/src/utils.py
@@ -1,28 +1,52 @@
 import time as t
 import math
+import numpy as np
 import cv2
 import random
 
-class time:
+class Time:
     # Measuring time
-
     def __init__(self):
         self.start_time = t.time()
 
     def elapsed(self):
         return t.time() - self.start_time
 
-def drawKeypoints(frame, kp):
+def drawKeypoints(frame, kps, BGR):
+    for i in range(len(kps)):
+        for j in range(len(kps[i])):
+            x = math.floor(kps[i, j, 0])
+            y = math.floor(kps[i, j, 1])
 
-
-    for i in range(0, len(kp), 1):
-        x = math.floor(kp[i][0])
-        y = math.floor(kp[i][1])
-
-        B = random.randrange(0, 255)
-        G = random.randrange(0, 255)
-        R = random.randrange(0, 255)
-
-        frame = cv2.circle(frame, (x, y), 2, (B, G, R))
+            frame = cv2.circle(frame, (x, y), 1, BGR, -1)
 
     return frame
+
+def videoFlow(kps, path, out_path, BGR):
+    kps = np.array(kps)
+
+    # Open video and get settings
+    video = cv2.VideoCapture(path)
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    width  = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps    = video.get(cv2.CAP_PROP_FPS)
+
+    # Open output video
+    output_video = cv2.VideoWriter(out_path, fourcc, fps, (width, height))
+
+    # Draw new images
+    for i in range(length):
+        # Get frame
+        ret, frame = video.read()
+
+        # Draw keypoints
+        frame = drawKeypoints(frame, kps[0:i,:,:], BGR)
+
+        # Write new image to video
+        output_video.write(frame)
+
+    # Close videos
+    video.release()
+    output_video.release()
