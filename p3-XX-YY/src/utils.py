2f55aebc31e0bb20a52efbfed0dc881743994618
Breno Leite <brenolleite@gmail.com>
2017-10-05 15:16:19 -0300

    Changing utils

--- p3-XX-YY/src/utils.py
+++ p3-XX-YY/src/utils.py
@@ -12,13 +12,13 @@ class Time:
     def elapsed(self):
         return t.time() - self.start_time
 
-def drawKeypoints(frame, kps, BGR):
+def drawKeypoints(frame, kps, BGR, size):
     for i in range(len(kps)):
         for j in range(len(kps[i])):
             x = math.floor(kps[i, j, 0])
             y = math.floor(kps[i, j, 1])
 
-            frame = cv2.circle(frame, (x, y), 1, BGR, -1)
+            frame = cv2.circle(frame, (x, y), size, BGR, -1)
 
     return frame
 
