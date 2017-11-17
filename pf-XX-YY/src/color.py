f4a67b13ef88c2fd1665a5d3bc9e3741b678b24f
Breno Leite <brenolleite@gmail.com>
2017-11-17 21:52:29 -0200

    Commiting same color

--- pf-XX-YY/src/color.py
+++ pf-XX-YY/src/color.py
@@ -14,17 +14,30 @@ def initColors():
 
     # Yellow
 
-def detectByColor(frame):
+def filterLargerComponent(areas, treshold = None):
+    if not treshold:
+        largest_area = 0
+        largest_i = -1
 
-    initColors()
+        # Goes over all region's label, skiping the 0 one, since it is background
+        for i in range(1, len(areas)):
+            if areas[i] > largest_area:
+                largest_area = areas[i]
+                largest_i = i
+
+        indexes = [largest_i]
+    else:
+        indexes = np.where(np.logical_and(areas > treshold, areas < treshold * 10))[0]
+
+    return indexes
+
+def detectByColor(frame):
     output = []
 
     # Transform the color space into HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     for i in range(len(hsvColor)):
-
-        print("1")
         # Gets a black and white image, all the pixels in the color range will be
         # painted white, everything else will be black
         mask = cv2.inRange(hsv, (hsvColor[i] - 20, 50, 0), (hsvColor[i] + 20, 255, 255))
@@ -39,23 +52,13 @@ def detectByColor(frame):
         _, pixelsLabel, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 
         # Gets the index of the largest connected component
-        componentIndex = filterLargerComponent(stats[:,4])
+        # stats[:, 4] gets all the areas of components
+        indexes = filterLargerComponent(stats[:,4], 3000)
 
-        x1, y1, x2, y2, _ = stats[componentIndex]
-        output.append((i, x1, y1, x1 + x2, y1 + y2, int(centroids[componentIndex][0]), int(centroids[componentIndex][1])))
+        for ix in indexes:
+            x1, y1, x2, y2, _ = stats[ix]
+            output.append((i, x1, y1, x1 + x2, y1 + y2, int(centroids[ix][0]), int(centroids[ix][1])))
 
     return output
 
-def filterLargerComponent(status):
-
-    larger = 0
-    largerIndex = -1
-
-    # Goes over all region's label, skiping the 0 one, since it is background
-    for i in range(1, len(status)):
-
-        if (status[i] > larger):
-            larger = status[i]
-            largerIndex = i
-
-    return largerIndex
+initColors()
